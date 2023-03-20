7730a51931baddd57f9ac8d25b274352e0a92767
Ekaterina Drobysheva <katerina_1_777@list.ru>
2023-03-20 23:30:20 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    public Map<String, String> contactsList = new HashMap<>();
+
+    public int add(String name, String phoneNumber) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,30 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+    PhoneBook phoneBook = new PhoneBook();
+
+    @BeforeEach
+    public void testStarted() {
+        System.out.println("The beginning of the test");
+    }
+
+    @AfterEach
+    public void testFinished() {
+        System.out.println("End of the test");
+    }
+
+    @Test
+    public void testAddContact() {
+        int expected = phoneBook.add("Misha", "+79851754235");
+        expected++;
+        Assertions.assertEquals(expected, phoneBook.add("Masha", "+79259754875"));
+    }
+
+    @Test
+    public void testWhenFirstNameIsNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> phoneBook.add(null, "+79871723414"));
+    }
 }
