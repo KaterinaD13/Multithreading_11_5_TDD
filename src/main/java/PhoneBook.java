ec865c8f7919c990ce50043fbe7886e7bde1142d
Ekaterina Drobysheva <katerina_1_777@list.ru>
2023-03-20 23:58:17 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -5,6 +5,13 @@ public class PhoneBook {
     public Map<String, String> contactsList = new HashMap<>();
 
     public int add(String name, String phoneNumber) {
-        return 0;
+        if (!contactsList.containsValue(name)) {
+            contactsList.put(name, phoneNumber);
+        } else {
+            System.out.println("There is already a contact with this name");// Контакт с таким именем уже есть
+        }
+        if (name == null)
+            throw new RuntimeException("The name cannot be null");// Имя не может быть нулевым
+        return contactsList.size();
     }
 }
